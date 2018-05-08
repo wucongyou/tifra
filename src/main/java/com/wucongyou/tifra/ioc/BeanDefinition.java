502d44a4585270cb54e3b458664f922354bd531e
congyou.wu <echo940811@163.com>
2018-05-08 23:59:21 +0800

    add BeanDefinition def

--- src/main/java/com/wucongyou/tifra/ioc/BeanDefinition.java
+++ src/main/java/com/wucongyou/tifra/ioc/BeanDefinition.java
@@ -5,4 +5,10 @@ package com.wucongyou.tifra.ioc;
  * @since 2018-05-08 下午10:47
  */
 public interface BeanDefinition {
+
+    void setBeanClassName(String beanClassName);
+
+    String getBeanClassName();
+
+    String[] getDependsOn();
 }
