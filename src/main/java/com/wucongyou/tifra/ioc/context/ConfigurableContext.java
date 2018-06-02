021d44905b8219d57943e2dc612a1a0971993eb0
congyou.wu <echo940811@163.com>
2018-06-02 14:28:25 +0800

    restart

--- src/main/java/com/wucongyou/tifra/ioc/context/ConfigurableContext.java
+++
@@ -1,8 +0,0 @@
-package com.wucongyou.tifra.ioc.context;
-
-/**
- * @author congyou.wu
- * @since 2018-05-08 下午10:33
- */
-public interface ConfigurableContext extends Context, LifeCycle, Cloneable {
-}
